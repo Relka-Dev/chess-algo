27048d4524629b5cda8a6922c2c00f8af9ed37e7
karelvil.svoboda <karel-vilem.svoboda@students.hevs.ch>
2025-12-18 13:07:58 +0100

    code: basic struct

--- ISChess/Bots/Martin.py
+++ ISChess/Bots/Martin.py
@@ -17,12 +17,6 @@ def chess_bot(player_sequence, board, time_budget, **kwargs):
     for x in range(board.shape[0]-1):
          for y in range(board.shape[1]):
             # Making a tree out of every piece
-            
-            if color in board[x,y]:  # Not a player piece
-                continue
-
-            
-
             possible_mov([x,y], board)
 
 
@@ -31,18 +25,63 @@ def chess_bot(player_sequence, board, time_budget, **kwargs):
 # Fonction récursive
 def possible_mov(piece_pos, board):
     piece = board[piece_pos][0]
+    color = board[piece_pos][1]
 
     match piece:
-        case 'k':    
-            return
-        case 'q':
-            return
         case 'p':
-            return
+            moves = pawn_moves(piece_pos, board, color)
         case 'r':
-            return 
+            moves = rook_moves(piece_pos, board, color)
+        case 'n':
+            moves = knight_moves(piece_pos, board, color)
+        case 'b':
+            moves = bishop_moves(piece_pos, board, color)
+        case 'q':
+            moves = queen_moves(piece_pos, board, color)
+        case 'k':
+            moves = king_moves(piece_pos, board, color)
+
+def pawn_moves(piece_pos, board, color):
+    x, y = piece_pos
+    moves = []
+    
+    if color == 'w':
+        if board[x + 1, y] == ' ':
+            moves.append((x + 1, y))
+        if y > 0 and board[x + 1, y - 1] == 'b':
+            moves.append((x + 1, y - 1))
+        if y < board.shape[1] - 1 and board[x + 1, y + 1] == 'b':
+            moves.append((x + 1, y + 1))
+    elif color == 'b': 
+        if board[x - 1, y] == ' ':
+            moves.append((x - 1, y))
+        if y > 0 and board[x - 1, y - 1] == 'w':
+            moves.append((x - 1, y - 1))
+        if y < board.shape[1] - 1 and board[x - 1, y + 1] == 'w':
+            moves.append((x - 1, y + 1))
+    
+    return moves
+
+def rook_moves(piece_pos, board):
+    x, y = piece_pos
 
 
+def knight_moves(piece_pos, board):
+    
+    pos_mov = [()]
+
+def bishop_moves(piece_pos, board):
+    pass
+
+def queen_moves(piece_pos, board):
+    pass
+
+def king_moves(piece_pos, board):
+    pass
+
+
+def board_evaluation(board, color):
+    pass
     
 #   Example how to register the function
 register_chess_bot("Martin", chess_bot)
