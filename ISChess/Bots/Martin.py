0e2935964b6f9c67dde54f4a38469b88e0de51ef
karelvil.svoboda <karel-vilem.svoboda@students.hevs.ch>
2025-12-18 10:38:18 +0100

    Struct: Fixed the issue with the base code ISChess

--- ISChess/Bots/Martin.py
+++ ISChess/Bots/Martin.py
@@ -26,21 +26,6 @@ def chess_bot(player_sequence, board, time_budget, **kwargs):
             possible_mov([x,y], board)
 
 
-            
-    
-    # for x in range(board.shape[0]-1):
-    #     for y in range(board.shape[1]):
-    #         if board[x,y] != "p"+color:
-    #             continue
-    #         if y > 0 and board[x+1,y-1] != '' and board[x+1,y-1][-1] != color:
-    #             return (x,y), (x+1,y-1)
-    #         if y < board.shape[1] - 1 and board[x+1,y+1] != '' and board[x+1,y+1][1] != color:
-    #             return (x,y), (x+1,y+1)
-    #         elif board[x+1,y] == '':
-    #             return (x,y), (x+1,y)
-
-
-
     return (0,0), (0,0) 
 
 # Fonction récursive
